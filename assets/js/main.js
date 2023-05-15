fef6c27fe18f4aa9a3b9878c117639c82c5eb9ec
aqurds <omarf1320@gmail.com>
2023-05-15 19:09:20 +0600

    bug fix in css V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
-let targetElement = document.getElementById("logo-icon");
-// console.log(targetElement);
-targetElement.addEventListener("click", openMobileMenu)
+// Define state in constant 
+const State = ["open", "close"];
 
-function openMobileMenu() {
-    console.log("clicked");
-    document.getElementById("mobile-menu").style.display = "block";
+// Open mobile menu 
+document.getElementById("logo-icon").addEventListener("click", () => {toggleMobileMenu(State[0])})
+
+// Close mobile menu by clicking on close button and menu items
+let closeElem = document.getElementsByClassName("close-mobile-menu");
+
+for (let i = 0; i < closeElem.length; i++) {
+    closeElem[i].addEventListener("click",  () => {toggleMobileMenu(State[1])});
 }
 
-let closeTargetElem = document.getElementById("close-mobile-menu");
-closeTargetElem.addEventListener("click", closeMobileMenu);
 
-function closeMobileMenu() {
-    console.log("clicked");
-    document.getElementById("mobile-menu").style.display = "none";
+// Mobile menu toggle function, will act on parameter. Also apply text blur effect based on figma design.
+function toggleMobileMenu(state) {
+    let textBlurElem = document.getElementsByClassName("section-intro")[0],
+        textBlueLogo = document.getElementById("logo-div"),
+        classApplicable = "text-filter";
+    if (state == State[0]) {
+        document.getElementById("mobile-menu").style.display = "block";
+        textBlurElem.classList.add(classApplicable);
+        textBlueLogo.classList.add(classApplicable);
+    } else if (state == State[1]) {
+        document.getElementById("mobile-menu").style.display = "none";
+        textBlurElem.classList.remove(classApplicable);
+        textBlueLogo.classList.remove(classApplicable);
+    }
 }
